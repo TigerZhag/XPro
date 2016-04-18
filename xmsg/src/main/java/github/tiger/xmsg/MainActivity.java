deee314f5c6bf770a49edbe5e2a263b234c1e114
Tiger <Tiger.zhag@gmail.com>
2016-04-19 03:13:45 +0800

    The first test push

--- xmsg/src/main/java/github/tiger/xmsg/MainActivity.java
+++ xmsg/src/main/java/github/tiger/xmsg/MainActivity.java
@@ -3,6 +3,7 @@ package github.tiger.xmsg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//An project for XingZiYang to graduate
 public class MainActivity extends AppCompatActivity {
 
     @Override
